6fb506c4ad59e5e364863e9eee863fe541c8c046
BarZaborowski <bartek.zaborowski1@gmail.com>
2026-09-23 11:16:35 +0200

    zmiany
    Refactor MainWindow logic to remove redundant variable declarations and streamline dice click handling.

--- Gra w kości 2026 desktop/MainWindow.xaml.cs
+++ Gra w kości 2026 desktop/MainWindow.xaml.cs
@@ -19,124 +19,242 @@ namespace Gra_w_kości_2026_desktop
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+{
+    bool dostepna1 = true;
+    bool dostepna2 = true;
+    bool dostepna3 = true;
+    bool dostepna4 = true;
+    bool dostepna5 = true;
+
+    int w1 = 0;
+    int w2 = 0;
+    int w3 = 0;
+    int w4 = 0;
+    int w5 = 0;
+    public MainWindow()
     {
-        bool dostepna1 = true;
-        bool dostepna2 = true;
-        bool dostepna3 = true;
-        bool dostepna4 = true;
-        bool dostepna5 = true;
-        public MainWindow()
+        InitializeComponent();
+        
+    }
+
+    private void kosc1_Click(object sender, RoutedEventArgs e)
+    {
+        if(dostepna1 == true)
         {
-            InitializeComponent();
-            
+            dostepna1 = false;
+            kosc1.Opacity = 0.5;
         }
-
-        private void kosc1_Click(object sender, RoutedEventArgs e)
+        else
         {
-            if(dostepna1 == true)
-            {
-                dostepna1 = false;
-                kosc1.Opacity = 0.5;
-            }
-            else
-            {
-                dostepna1 = true;
-                kosc1.Opacity = 1;
-            }
-        }
-
-        private void kosc2_Click(object sender, RoutedEventArgs e)
-        {
-            if (dostepna2 == true)
-            {
-                dostepna2 = false;
-                kosc2.Opacity = 0.5;
-            }
-            else
-            {
-                dostepna2 = true;
-                kosc2.Opacity = 1;
-            }
-        }
-
-        private void kosc3_Click(object sender, RoutedEventArgs e)
-        {
-            if (dostepna3 == true)
-            {
-                dostepna3 = false;
-                kosc3.Opacity = 0.5;
-            }
-            else
-            {
-                dostepna3 = true;
-                kosc3.Opacity = 1;
-            }
-        }
-
-        private void kosc4_Click(object sender, RoutedEventArgs e)
-        {
-            if (dostepna4 == true)
-            {
-                dostepna4 = false;
-                kosc4.Opacity = 0.5;
-            }
-            else
-            {
-                dostepna4 = true;
-                kosc4.Opacity = 1;
-            }
-        }
-
-        private void kosc5_Click(object sender, RoutedEventArgs e)
-        {
-            if (dostepna5 == true)
-            {
-                dostepna5 = false;
-                kosc5.Opacity = 0.5;
-            }
-            else
-            {
-                dostepna5 = true;
-                kosc5.Opacity = 1;
-            }
-        }
-
-        private void przycisk_Click(object sender, RoutedEventArgs e)
-        {
-            Random l1 = new Random();
-            Random l2 = new Random();
-            Random l3 = new Random();
-            Random l4 = new Random();
-            Random l5 = new Random();
-            int w1 = 0;
-            int w2 = 0;
-            int w3 = 0;
-            int w4 = 0;
-            int w5 = 0;
-            if (dostepna1 == true)
-            {
-                w1 = l1.Next(1, 7);
-                
-            }
-            if (dostepna2 == true)
-            {
-                w2 = l2.Next(1, 7);
-            }
-            if (dostepna3 == true)
-            {
-                w3 = l3.Next(1, 7);
-            }
-            if (dostepna4 == true)
-            {
-                w4 = l4.Next(1, 7);
-            }
-            if (dostepna5 == true)
-            {
-                w5 = l5.Next(1, 7);
-            }
-
-            int suma = (w1 + w2 + w3 + w4 + w5);
-            Wynik.Text = suma.ToString();
+            dostepna1 = true;
+            kosc1.Opacity = 1;
         }
     }
+
+    private void kosc2_Click(object sender, RoutedEventArgs e)
+    {
+        if (dostepna2 == true)
+        {
+            dostepna2 = false;
+            kosc2.Opacity = 0.5;
+        }
+        else
+        {
+            dostepna2 = true;
+            kosc2.Opacity = 1;
+        }
+    }
+
+    private void kosc3_Click(object sender, RoutedEventArgs e)
+    {
+        if (dostepna3 == true)
+        {
+            dostepna3 = false;
+            kosc3.Opacity = 0.5;
+        }
+        else
+        {
+            dostepna3 = true;
+            kosc3.Opacity = 1;
+        }
+    }
+
+    private void kosc4_Click(object sender, RoutedEventArgs e)
+    {
+        if (dostepna4 == true)
+        {
+            dostepna4 = false;
+            kosc4.Opacity = 0.5;
+        }
+        else
+        {
+            dostepna4 = true;
+            kosc4.Opacity = 1;
+        }
+    }
+
+    private void kosc5_Click(object sender, RoutedEventArgs e)
+    {
+        if (dostepna5 == true)
+        {
+            dostepna5 = false;
+            kosc5.Opacity = 0.5;
+        }
+        else
+        {
+            dostepna5 = true;
+            kosc5.Opacity = 1;
+        }
+    }
+
+    private void przycisk_Click(object sender, RoutedEventArgs e)
+    {
+        Random l = new Random();
+        
+        
+        if (dostepna1 == true)
+        {
+            w1 = l.Next(1, 7);
+            if(w1 == 1)
+            {
+                ob1.Source = new BitmapImage(new Uri("zdjecia/kosc1.png", UriKind.Relative));
+            }
+            if (w1 == 2)
+            {
+                ob1.Source = new BitmapImage(new Uri("zdjecia/kosc2.png", UriKind.Relative));
+            }
+            if (w1 == 3)
+            {
+                ob1.Source = new BitmapImage(new Uri("zdjecia/kosc3.png", UriKind.Relative));
+            }
+            if (w1 == 4)
+            {
+                ob1.Source = new BitmapImage(new Uri("zdjecia/kosc4.png", UriKind.Relative));
+            }
+            if (w1 == 5)
+            {
+                ob1.Source = new BitmapImage(new Uri("zdjecia/kosc5.png", UriKind.Relative));
+            }
+            if (w1 == 6)
+            {
+                ob1.Source = new BitmapImage(new Uri("zdjecia/kosc6.png", UriKind.Relative));
+            }
+        }
+        if (dostepna2 == true)
+        {
+            w2 = l.Next(1, 7);
+            if (w2 == 1)
+            {
+                ob2.Source = new BitmapImage(new Uri("zdjecia/kosc1.png", UriKind.Relative));
+            }
+            if (w2 == 2)
+            {
+                ob2.Source = new BitmapImage(new Uri("zdjecia/kosc2.png", UriKind.Relative));
+            }
+            if (w2 == 3)
+            {
+                ob2.Source = new BitmapImage(new Uri("zdjecia/kosc3.png", UriKind.Relative));
+            }
+            if (w2 == 4)
+            {
+                ob2.Source = new BitmapImage(new Uri("zdjecia/kosc4.png", UriKind.Relative));
+            }
+            if (w2 == 5)
+            {
+                ob2.Source = new BitmapImage(new Uri("zdjecia/kosc5.png", UriKind.Relative));
+            }
+            if (w2 == 6)
+            {
+                ob2.Source = new BitmapImage(new Uri("zdjecia/kosc6.png", UriKind.Relative));
+            }
+        }
+        if (dostepna3 == true)
+        {
+            w3 = l.Next(1, 7);
+            if (w3 == 1)
+            {
+                ob3.Source = new BitmapImage(new Uri("zdjecia/kosc1.png", UriKind.Relative));
+            }
+            if (w3 == 2)
+            {
+                ob3.Source = new BitmapImage(new Uri("zdjecia/kosc2.png", UriKind.Relative));
+            }
+            if (w3 == 3)
+            {
+                ob3.Source = new BitmapImage(new Uri("zdjecia/kosc3.png", UriKind.Relative));
+            }
+            if (w3 == 4)
+            {
+                ob3.Source = new BitmapImage(new Uri("zdjecia/kosc4.png", UriKind.Relative));
+            }
+            if (w3 == 5)
+            {
+                ob3.Source = new BitmapImage(new Uri("zdjecia/kosc5.png", UriKind.Relative));
+            }
+            if (w3 == 6)
+            {
+                ob3.Source = new BitmapImage(new Uri("zdjecia/kosc6.png", UriKind.Relative));
+            }
+        }
+        if (dostepna4 == true)
+        {
+            w4 = l.Next(1, 7);
+            if (w4 == 1)
+            {
+                ob4.Source = new BitmapImage(new Uri("zdjecia/kosc1.png", UriKind.Relative));
+            }
+            if (w4 == 2)
+            {
+                ob4.Source = new BitmapImage(new Uri("zdjecia/kosc2.png", UriKind.Relative));
+            }
+            if (w4 == 3)
+            {
+                ob4.Source = new BitmapImage(new Uri("zdjecia/kosc3.png", UriKind.Relative));
+            }
+            if (w4 == 4)
+            {
+                ob4.Source = new BitmapImage(new Uri("zdjecia/kosc4.png", UriKind.Relative));
+            }
+            if (w4 == 5)
+            {
+                ob4.Source = new BitmapImage(new Uri("zdjecia/kosc5.png", UriKind.Relative));
+            }
+            if (w4 == 6)
+            {
+                ob4.Source = new BitmapImage(new Uri("zdjecia/kosc6.png", UriKind.Relative));
+            }
+        }
+        if (dostepna5 == true)
+        {
+            w5 = l.Next(1, 7);
+            if (w5 == 1)
+            {
+                ob5.Source = new BitmapImage(new Uri("zdjecia/kosc1.png", UriKind.Relative));
+            }
+            if (w5 == 2)
+            {
+                ob5.Source = new BitmapImage(new Uri("zdjecia/kosc2.png", UriKind.Relative));
+            }
+            if (w5 == 3)
+            {
+                ob5.Source = new BitmapImage(new Uri("zdjecia/kosc3.png", UriKind.Relative));
+            }
+            if (w5 == 4)
+            {
+                ob5.Source = new BitmapImage(new Uri("zdjecia/kosc4.png", UriKind.Relative));
+            }
+            if (w5 == 5)
+            {
+                ob5.Source = new BitmapImage(new Uri("zdjecia/kosc5.png", UriKind.Relative));
+            }
+            if (w5 == 6)
+            {
+                ob5.Source = new BitmapImage(new Uri("zdjecia/kosc6.png", UriKind.Relative));
+            }
+        }
+
+        int suma = (w1 + w2 + w3 + w4 + w5);
+        Wynik.Text = suma.ToString();
+    }
+}
 }
